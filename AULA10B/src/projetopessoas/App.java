769ac03a8f72f4f56858125112538cffca9bf8d4
Wellington Araujo <welles.araujo@hotmail.com>
2020-09-24 19:09:24 -0300

    Adicionado P5 e seus atributos

--- AULA10B/src/projetopessoas/App.java
+++ AULA10B/src/projetopessoas/App.java
@@ -6,11 +6,17 @@ public class App {
         Aluno p2 = new Bolsista();
         Professor p3 = new Professor();
         Funcionario p4 = new Funcionario();
+        Aluno p5 = new Aluno();
 
         p1.setNome("Pedro");
         p2.setNome("Maria");
         p3.setNome("Claudio");
         p4.setNome("Fabiana");
+
+        p5.setNome("Carlinhos");
+        p5.setIdade(25);
+        p5.setCurso("Comptucação");
+        p5.setMatr(1235);
 
         p1.pagarMensal();
         p2.pagarMensal();
